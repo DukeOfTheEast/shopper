6b08256226cc045a4904b9f8035dd55d4e0fd50d
Ukachukwu Ebuka <fralexander1999@gmail.com>
2024-09-15 16:04:13 +0100

    fdgdh

--- src/app/cart/checkoutSuccess/page.jsx
+++ src/app/cart/checkoutSuccess/page.jsx
@@ -8,26 +8,23 @@ import { ArrowLeft } from "lucide-react";
 
 const CheckoutSuccess = () => {
   return (
-    <div className="flex flex-col items-center text-center h-screen py-10 bg-gradient-to-r from-green-100 via-white to-green-100">
+    <div className="flex flex-col items-center text-center h-full sm:h-screen py-10 bg-gradient-to-r from-green-100 via-white to-green-100">
       <p className="font-bold">Thank you for your purchase!</p>
       <Image src={Checkout} alt="checkout" width={300} height={300} />
       <p>Thanks for placing an order:</p>
       <p className="my-3 mx-3">
         We will send you a notification within 5 days when it ships.
       </p>
-      <p className="mx-3">
-        {" "}
-        If you have any questions or queries, please do not fail to contact us.
-      </p>
+
       <p>All the Best</p>
-      <div className="flex gap-3 my-8">
+      <div className="flex sm:flex-row flex-col-reverse gap-3 my-8 w-[300px] sm:w-auto">
         <Link href={"/products"}>
-          <button className="flex items-center gap-1 rounded-md py-1 px-3 border border-green-500 text-sm font-serif">
+          <button className="flex items-center justify-center gap-1 rounded-md py-1 px-3 border border-green-500 font-serif w-full sm:w-auto">
             <ArrowLeft size={15} />
             <p>Continue Shopping</p>
           </button>
         </Link>
-        <button className="rounded-md py-1 px-3 bg-green-500 text-white text-sm">
+        <button className="rounded-md py-1 px-3 bg-green-500 text-white w-full sm:w-auto">
           Download as PDF
         </button>
       </div>
