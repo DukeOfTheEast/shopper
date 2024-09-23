232cd8567fa85cb5ec942f5f2b6ac27596c14c6a
Ukachukwu Ebuka <fralexander1999@gmail.com>
2024-09-23 18:04:03 +0100

    gdgdg

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -3,10 +3,11 @@ import SignupImage from "@/images/signup-img.jpg";
 import Image from "next/image";
 import Google from "@/images/google.png";
 import Facebook from "@/images/facebook.png";
+import Link from "next/link";
 
 const Signup = () => {
   return (
-    <div className="flex p-3">
+    <div className="flex sm:p-0 p-3">
       <Image
         src={SignupImage}
         alt="signup"
@@ -14,20 +15,23 @@ const Signup = () => {
         height={300}
         className="w-1/2 h-screen hidden md:block"
       />
-      <div className="flex flex-col justify-center md:w-1/2 md:px-36 mt-5">
-        <h1 className="font-extrabold text-center text-green-500 text-3xl">
+      <div className="flex flex-col justify-center md:w-1/2 md:px-36 mt-5 sm:mt-2">
+        <h1 className="sm:hidden font-extrabold text-center sm:text-left text-green-500 text-3xl">
           Shop with Purpose:
           <br /> Join our Shopping Revolution
+        </h1>
+        <h1 className="hidden sm:block font-extrabold text-center sm:text-left text-green-500 text-3xl">
+          Shop with Purpose: Join our Shopping Revolution
         </h1>
         <p className="my-2 font-bold text-xs text-center">
           Enter your credentials to create an account.
         </p>
         <div className="flex text-xs md:my-2 gap-2 mt-3">
-          <div className="flex border border-gray-300 gap-2 py-3 md:px-8 px-4 rounded-xl cursor-pointer">
+          <div className="flex border border-gray-300 gap-2 py-3 md:px-6 px-4 rounded-xl cursor-pointer">
             <Image src={Google} width={15} height={15} alt="google" />
             <p>Login with Google</p>
           </div>
-          <div className="flex border border-gray-300 gap-2 py-3 md:px-8 px-4 rounded-xl cursor-pointer">
+          <div className="flex border border-gray-300 gap-2 py-3 md:px-6 px-4 rounded-xl cursor-pointer">
             <Image src={Facebook} width={15} height={15} alt="facebook" />
             <p>Login with Facebook</p>
           </div>
@@ -123,6 +127,12 @@ const Signup = () => {
             </button>
           </div>
         </form>
+        <p className="text-sm my-2">
+          Already have an account?{" "}
+          <span className="text-green-500 font-bold">
+            <Link href={"/login"}>Login</Link>
+          </span>
+        </p>
       </div>
     </div>
   );
