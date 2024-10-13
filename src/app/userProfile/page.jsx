7fe16b7798f95351360f2e9fd9930d8dd4f302d9
Ukachukwu Ebuka <fralexander1999@gmail.com>
2024-10-13 13:43:54 +0100

    history

--- src/app/userProfile/page.jsx
+++ src/app/userProfile/page.jsx
@@ -8,6 +8,8 @@ import { db, storage } from "@/app/firebase/config";
 import { doc, setDoc, getDoc } from "firebase/firestore";
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { useProfile } from "@/context/Profile/page";
+import { UploadIcon } from "lucide-react";
+import Navbar from "@/components/navbar/page";
 
 const UserProfile = () => {
   const { currentUser } = useAuth();
@@ -83,25 +85,50 @@ const UserProfile = () => {
 
   return (
     <div>
-      <input
-        type="file"
-        id="file-input"
-        onChange={handleImageChange}
-        className="sr-only"
-      />
-      <label htmlFor="file-input" className="cursor-pointer">
-        Upload
-      </label>
-      {currentUser && (
-        <Image
-          src={currentUser.photoURL || photoURL}
-          alt="profile"
-          height={300}
-          width={300}
-          priority
-          className="rounded-full"
-        />
-      )}
+      <Navbar />
+      <div className="flex flex-col items-center justify-center">
+        <div className="flex flex-row-reverse items-center gap-4 mt-20 mb-5">
+          <div className="">
+            <input
+              type="file"
+              id="file-input"
+              onChange={handleImageChange}
+              className="sr-only"
+            />
+
+            <label
+              htmlFor="file-input"
+              className="cursor-pointer block bg-green-500 text-white px-5 py-2 rounded-xl gap-2 flex"
+            >
+              <UploadIcon size={25} />
+              <p>Upload</p>
+            </label>
+          </div>
+          {currentUser && (
+            <Image
+              src={currentUser.photoURL || photoURL}
+              alt="profile"
+              height={300}
+              width={300}
+              priority
+              className="rounded-full w-40 h-40"
+            />
+          )}
+        </div>
+        <div className="mb-2">
+          <p>Name: {currentUser.fullName}</p>
+          <p>Email: {currentUser.email}</p>
+        </div>
+      </div>
+
+      <hr className="w-3/4 mx-auto" />
+      <hr className="w-3/4 mx-auto" />
+      <div className="mx-4 my-6">
+        <h1 className="font-bold text-green-500">History</h1>
+        <div className="flex items-center justify-center italic">
+          <p>No activities yet</p>
+        </div>
+      </div>
     </div>
   );
 };
